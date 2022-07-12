1ea78f4f1327c9a85c76e11727f1e76cddf39990
Madeline <grammaticomadeline@hotmail.fr>
2022-07-12 16:33:17 +0200

    fix(app) : fix modal keypress

--- scripts/model/Modal.js
+++ scripts/model/Modal.js
@@ -5,6 +5,8 @@ export default class Modal {
     this._handlerCloseModal = this.closeModal.bind(this);
     this._handlerKeyclose = this.keyClose.bind(this);
     this._form = this.modal.querySelector('form');
+    this.header = document.querySelector('header');
+    this.main = document.querySelector('#main');
   }
 
   get name() {
@@ -31,6 +33,8 @@ export default class Modal {
   displayModal() {
     this.modal.style.display = 'flex';
     this.modal.ariaHidden = 'false';
+    this.header.style.display = 'none';
+    this.main.style.display = 'none';
     this.body.style.overflow = 'hidden';
     const iconClose = document.querySelector('.modal header img');
     this._form.addEventListener('submit', this.submitModal.bind(this));
@@ -41,6 +45,8 @@ export default class Modal {
   closeModal() {
     this.modal.ariaHidden = 'true';
     this.modal.style.display = 'none';
+    this.header.style.display = 'block';
+    this.main.style.display = 'block';
     this._form.removeEventListener('submit', this.submitModal.bind(this));
     document.removeEventListener('keydown', this._handlerKeyclose);
     const iconClose = document.querySelector('.modal header img');
